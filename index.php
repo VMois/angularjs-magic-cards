e8a50645b459a063a71d7f70a15caca019242a07
vmois <vladyslav.moisieienkov@gmail.com>
2017-11-25 11:26:55 +0100

    Add base styling of main page

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php 
-require("connection.php");
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,9 +6,10 @@ require("connection.php");
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
     </head>
-    <body ng-app="mcardsApp">
+    <body class="root_div" ng-app="mcardsApp">
         <div ng-view></div>
         <script src="js/settings.js"></script>
         <script src="js/main.js"></script>
+        <link rel="stylesheet" href="css/reset.css">
     </body>
 </html>
