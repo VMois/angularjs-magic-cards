8520da6dd8079f6de42968913890250c7e31008a
vmois <vladyslav.moisieienkov@gmail.com>
2017-11-25 10:50:06 +0100

    Add connection and index php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+require("connection.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Magic Cards - Main</title>
+        <meta charset="UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
